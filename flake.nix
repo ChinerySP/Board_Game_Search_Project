c4aecbdbf4807c2e16c9caef6ae0dadcc73bcb6c
Sa9m <abyssalflerken@gmail.com>
2026-03-18 12:14:46 -0500

    Added nixos development env

---
+++ flake.nix
@@ -0,0 +1,28 @@
+# This is specifically for Sam because they use NixOS, if you don't, don't worry about this file :)
+{
+  description = "Java NixOS flake for this project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [
+          javaPackages.compiler.openjdk21
+          jdt-language-server
+        ];
+        buildInputs = with pkgs; [];
+      in {
+        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+      }
+    );
+}
