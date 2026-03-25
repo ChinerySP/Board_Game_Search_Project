e85d640b7b2155d24fb3174b021ae0e9240dba84
ChinerySP <123766728+ChinerySP@users.noreply.github.com>
2026-03-25 12:33:17 -0500

    Added initial templates for User and Config

---
+++ Main/Model/Config.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Config {
+
+    private bool apiActive;
+
+    /**
+     * Sets the API status to determine if user is in offline or online mode
+     * @param apiActive Used to determine if the API is used or not
+     * @return returns the current status of the API
+     * True for online, False for offline
+     */
+    public bool setAPIActive(bool apiActive) {
+        //Online/offline mode toggled in settings?
+    }
+}
--- Main/Model/User.java
+++ Main/Model/User.java
@@ -1,1 +1,60 @@
-// Spencer this is on you
+public class User {
+
+    /**
+     * A collection of GameLists specific to the user
+     */
+    public GameLists() {
+
+    }
+
+    /**
+     * Used to reset the current password for the user
+     * @param newPassword The new password chosen by the user
+     */
+    public void resetPassword(String newPassword) {
+        password = newPassword;
+    }
+
+    /**
+     * The user will input a password in order to log in
+     * to their account
+     * @param guess The password input by the user
+     * @return True if the password is correct, False if incorrect
+     */
+    public bool login(String guess) {
+        if (guess == password) {
+            return True;
+        }
+        else return False;
+    }
+
+    /**
+     * Uses View to prompt user if they would like to save data
+     * upon logout
+     * @return User either chooses to logout with our without saving data
+     * True for save, False for not saved
+     */
+    public bool logout() {
+
+    }
+
+    /**
+     * User data is loaded
+     * @return
+     */
+    public bool loadUserData() {
+
+    }
+
+    /**
+     * Used by logout() to save user data upon logout based
+     *  on user choice
+     * @return True for save data and False for not saved
+     */
+    public bool saveUserData() {
+
+    }
+
+    private String password;
+    private Config config;
+}
