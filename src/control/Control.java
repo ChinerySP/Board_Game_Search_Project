622e02e85667f1987e65a5e97484715c63583342
Sa9m <abyssalflerken@gmail.com>
2026-04-11 23:16:45 -0500

    Added some initial MVC implementation to communicate with View

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,25 +1,77 @@
 package control;
 
+import view.*;
+import model.*;
+
 public class Control {
 
+
+    // Testing function for the controller
     public static void main(String[] args) {
 
-        View view = new View();
-        Model model = new Model();
+        // Creating the controller
+        Control controller = new Control();
 
-        do {
-            // returns an array of username[0] and password
-            String[] inputArgs = view.login();
-        }
-        while(DataBase.hasUser(inputArgs));
+    }
+    
+    /**
+     * Constructor, begins the app itself
+     */
+    public Control() {
 
-        view.openDash(model.getActiveUser());
-        view.setDatabase(DataBase);
+        // Initializing the other two branches
+        view = new View(this);
+        model = new Model();
+
+        // Beginning the view by asking for the login from the view
+        view.promptLogin();
 
     }
 
-    public void Search(String keyword) {
-
+    /**
+     * Verifies the login information, and if it matches, tells the view to 
+     * open the dashboard
+     * @param String username The username of the user logging in 
+     * @param String password The password to check against
+     * @return User The user that logged in; null if there was no match
+     */
+    public User verifyLogin(String username, String password) {
+        // Todo update this to actually match the model
+        // return model.verifyLogin(username, password);
+        return null; // For testing 
     }
+
+    /**
+     * A forgot password method, skips verifying the login with a password
+     * @param 
+     */
+    public User forgotPassword(String username) {
+        // TODO update this to actually match the model
+        // return model.getUser(username);
+        return null; // For testing 
+    }
+
+    /**
+     * Gives a list of searching keywords to the database as a search querie.
+     * @param String[] keywords The keywords that we are searching for
+     * @return GameList A ordered GameList holding all of the results of the search
+     */
+    public GameList Search(String[] keywords) {
+        // TODO 
+        return null;
+    }
+
+    /**
+     * Tells the model to turn the API to a specified state
+     * @param boolean The state to set the API to
+     */
+    public void setAPI(boolean state) {
+        // TODO implement setAPI function for model
+        // model.setAPI(state);
+    }
+
+    // The view and the model, so that we can interface with them
+    private View view;
+    private Model model;
 
 }
