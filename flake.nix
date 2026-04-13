e5083d139229dd6aa206f1431b86fd979db805b3
Sa9m <abyssalflerken@gmail.com>
2026-04-13 15:29:09 -0500

    Updated flake.nix to actually work with intellij

--- flake.nix
+++ flake.nix
@@ -1,4 +1,55 @@
-# This is specifically for Sam because they use NixOS, if you don't, don't worry about this file :)
+# # # This is specifically for Sam because they use NixOS, if you don't, don't worry about this file :)
+# # {
+# #   description = "Java NixOS flake for this project";
+
+# #   inputs = {
+# #     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+# #     flake-utils.url = "github:numtide/flake-utils";
+# #   };
+
+# #   outputs = {
+# #     nixpkgs,
+# #     flake-utils,
+# #     ...
+# #   }:
+# #     flake-utils.lib.eachDefaultSystem (
+# #       system: let
+# #         pkgs = nixpkgs.legacyPackages.${system};
+
+# #         nativeBuildInputs = with pkgs; [
+# #           javaPackages.compiler.openjdk21
+# #           jdt-language-server
+# #           jdk
+# #           libX11
+# #           libXext
+# #           libXrender
+# #           libXtst
+# #           libXi
+# #         ];
+# #         buildInputs = with pkgs; [];
+# #       in {
+# #         devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+# #       }
+# #     );
+# # }
+# {
+#   outputs = { nixpkgs, flake-utils, ... }:
+#     flake-utils.lib.eachDefaultSystem (system: let
+#       pkgs = nixpkgs.legacyPackages.${system};
+#       jdk = pkgs.jdk;
+#       x11libs = with pkgs; [ libX11 libXext libXrender libXtst libXi ];
+#     in {
+#       devShells.default = pkgs.mkShell {
+#         packages = [ jdk ];
+#         buildInputs = x11libs;
+#         shellHook = ''
+#           export JAVA_HOME=${jdk.home}
+#           export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath x11libs}
+#         '';
+#       };
+#     });
+# }   
+
 {
   description = "Java NixOS flake for this project";
 
@@ -7,22 +58,39 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        nativeBuildInputs = with pkgs; [
-          javaPackages.compiler.openjdk21
-          jdt-language-server
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      jdk = pkgs.jdk25;
+      
+      # The libraries Java needs to draw GUIs
+      x11libs = with pkgs; [ 
+        libX11 
+        libXext 
+        libXrender 
+        libXtst 
+        libXi 
+      ];
+      
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [ 
+          jdk 
+          pkgs.jdt-language-server
         ];
-        buildInputs = with pkgs; [];
-      in {
-        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
-      }
-    );
+        
+        buildInputs = x11libs ++ [ pkgs.fontconfig ];
+        
+        shellHook = ''
+          export JAVA_HOME=${jdk.home}
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath x11libs}:$LD_LIBRARY_PATH
+
+          # Create a stable symlink to the Nix JDK in the project root
+          # This is the JDK that we want to be using for compilation
+          # it is Java 25 for
+          ln -sfn ${jdk.home} .nix-jdk
+          
+        '';
+      };
+    });
 }
