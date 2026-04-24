31afa57670ee4912ce6c2c11b4c9e78a3e334676
ChinerySP <123766728+ChinerySP@users.noreply.github.com>
2026-04-23 19:11:40 -0500

    Added functionality for ratings in User

--- src/model/User.java
+++ src/model/User.java
@@ -4,51 +4,94 @@ import model.*;
 
 public class User {
 
+    /**
+     * Default constructor for the User class
+     * userName is set to TJS and password is set to "password"
+     */
     public User() {
-        gameLists = new ArrayList<GameList>();
+        userLists = new ArrayList<GameList>();
+        userRatings = new ArrayList<Rating>();
         this.config = new Config();
         this.password = "password";
         this.userName = "TJS";
     }
 
+    /**
+     * Creates a new User with information provided to function
+     * @param user Contains the username that the new user will have
+     * @param password Contains the password that will be associated with the user
+     */
     public User(String user, String password) {
-        gameLists = new ArrayList<GameList>();
+        userLists = new ArrayList<GameList>();
+        userRatings = new ArrayList<Rating>();
         this.config = new Config();
         this.password = password;
         this.userName = user;
     }
 
-
+    /**
+     * Getter that returns the current username
+     * @return returns userName variabe
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Changes the current Username variable
+     * @param newName The new username that will be assigned to the variable userName
+     */
     public void setUserName(String newName) {
         userName = newName;
     }
 
+    /**
+     * Gives the current password associated with the user
+     * @return Returns the password variable
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Changes the current password variable
+     * @param newPassword the new password that will be assigned the variable "password"
+     */
     public void resetPassword(String newPassword) {
         password = newPassword;
     }
 
+    /**
+     * Adds a new list of games associated with the current user
+     * @param listName The name of the game list that is being created
+     */
     public void addGameList(String listName) {
-        gameLists.add(new GameList(listName));
+        userLists.add(new GameList(listName));
     }
 
+    /**
+     * Adds a currently created game list to the array "userLists"
+     * @param toAdd The GameList that will be added to the array
+     */
     public void addGameList(GameList toAdd) {
-        gameLists.add(toAdd);
+        userLists.add(toAdd);
     }
 
+    /**
+     * Changes the current state of the API activation
+     * @param state The state that will be assigned to "apiActive"
+     *              True for active, false for inactive
+     */
     public void setAPI(Boolean state) {
         config.setAPIActive(state);
     }
 
+    /**
+     * Gives the current list of games associated with the user
+     * @return Returns the array of game lists labeled "userLists"
+     */
     public ArrayList<GameList> getGameLists() {
-        return gameLists;
+        return userLists;
     }
 
     /**
@@ -61,16 +104,46 @@ public class User {
         return guess.equals(password);
     }
 
+    /**
+     * Changes the current state of "isDark" in the Config class
+     * @param state The state that will be assigned to variable "isDark"
+     */
     public void setDarkMode(boolean state) {
         config.setDarkMode(state);
     }
 
+    /**
+     * Gives the current state of the "isDark" variable
+     * @return Returns "isDark" from the config class
+     */
     public boolean getDarkMode() {
         return config.isDarkMode();
     }
 
+    /**
+     * Gives the current status of the API
+     * @return Returns the state of
+     */
     public boolean getAPI() {
         return config.isApiActive();
+    }
+
+    /**
+     * Gives the current rating for the specified game
+     * @param game The game that has been chosen to be rated
+     * @return Returns the rating in form aof a double
+     */
+    public double getRating(Game game) {
+        return game.getRating();
+    }
+
+    /**
+     * Sets a new rating for a specified game
+     * @param game The game that will be rated by the user
+     * @param newRating The new rating that will be used by the user
+     */
+    public void setRating(Game game, Rating newRating) {
+        game.rate(newRating);
     }
 
 
@@ -90,7 +163,8 @@ public class User {
         return true;
     }
 
-    private ArrayList<GameList> gameLists;
+    private ArrayList<Rating> userRatings;
+    private ArrayList<GameList> userLists;
     private String password;
     private Config config;
     private String userName;
